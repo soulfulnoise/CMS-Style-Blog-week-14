165b9f39521df50f5ac6f73bc668b3f8ef9e0ae5
The Noise <leespaulding25@gmail.com>
2021-03-27 02:06:01 -0400

    used fetch ti send post request to the API

--- public/js/login.js
+++ public/js/login.js
@@ -8,5 +8,15 @@ const password = document.querySelector('#password-login').nodeValue.trim();
 
 if ( email && password) {
     // Send post request to the API endpoint
+    const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+    });
 
-}
+    if (response.ok) {
+
+        document.location.replace('/profile');
+    } else {
+        alert(response.statusText);
+    }};
