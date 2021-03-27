b73bded61b3bf12c697ed35cd8f2cb6d5b6a5ff9
The Noise <leespaulding25@gmail.com>
2021-03-27 01:46:00 -0400

    collect  values from login form

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,12 @@
 const loginFormHandler = async (event) => {
     event.preventDefault();
 }
+
+// Collecting email and passwords entered in to login form
+const email = document.querySelector('#email-login').nodeValue.trim();
+const password = document.querySelector('#password-login').nodeValue.trim();
+
+if ( email && password) {
+    // Send post request to the API endpoint
+
+}
